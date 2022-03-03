918405470917cc30b6e17dee183ed9b3bd0b5c32
del0ct <letlluc@yandex.ru>
2022-03-03 15:26:08 +0300

    update 0.1.1

--- encrypt/main.cpp
+++ encrypt/main.cpp
@@ -5,12 +5,15 @@
 #include <tuple>
 #include <cmath>
 using namespace std;
-string pas1, pas2, pas3, res;
-vector <int> pus1, pus2, pus3;
+string pas1, pas2, pas3;
+int a;
+//vector <int> pus1, pus2, pus3;
 
-vector <int> strtovec(string str, vector <int> vec);
+vector <int> strtovec(string str);
 
-string vectostr(vector <int> vec, string str);
+vector <int> exp(string str);
+
+string vectostr(vector <int> vec);
 
 string pis(string a);
 
@@ -18,6 +21,11 @@ vector <int> shifr(vector <int> a, vector <int> b);
 
 vector <int> dis(vector <int> vec);
 
+string vectostrws(vector <int> vec);
+
+vector <int> strtovecws(string str);
+
+vector <int> shifrws(vector <int> a, vector <int> b);
 void entr()
 {
 	cout << "first (any english letters or numbers): ";
@@ -26,33 +34,28 @@ void entr()
 	cin >> pas2;
 	cout << "third (ONLY letters minimum 3, but preferably more): ";
 	cin >> pas3;
-}
-
-void exp()
-{
-	string p;
-	while (pas2.find('0') != -1)
-		pas2.erase(pas2.find('0'), 1);
-	if (pas2.length() % 2 != 0)
-		pas2.erase(pas2.length() - 1, 1);
-	for (int i = 0; i < (int)pas2.length(); i += 2)
-	{
-		p.assign(pas2, i, 2);
-		pus2.push_back(stoi(p));
-	}
+	cout << "symbol on?" << endl;
+	cin >> a;
 }
 
 int main()
 {
 	setlocale(0, "");
 	entr();
-	pus1 = strtovec(pas1, pus1); //first
-	exp(); // second
-	pas3 = pis(pas3); //thr
-	pus3 = strtovec(pas3, pus3); //thr
-	pus3 = dis(pus3); //thr
-	pus1 = shifr(pus1, pus2);
-	pus1 = shifr(pus1, pus3);
-	cout << vectostr(pus1, pas1) << endl;
+	//pus1 = strtovec(pas1, pus1); //first
+	//exp(); // second
+	//pas3 = pis(pas3); //thr
+	//pus3 = strtovec(pas3, pus3); //thr
+	//pus3 = dis(pus3); //thr
+	//pus1 = shifr(pus1, pus2);
+	//pus1 = shifr(pus1, pus3);
+	if (a == 1) 
+	{
+		cout << vectostrws(shifrws((shifrws(strtovecws(pas1), exp(pas2))), dis(strtovecws(pis(pas3))))) << endl;
+	}
+	else 
+	{
+		cout << vectostr(shifr((shifr(strtovec(pas1), exp(pas2))), dis(strtovec(pis(pas3))))) << endl;
+	}
 	system("pause");
 }
